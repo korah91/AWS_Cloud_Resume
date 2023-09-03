865098801228f3d4734334e5f4884a711e6d109c
korah91 <62379054+korah91@users.noreply.github.com>
2023-09-03 13:00:51 +0200

    - correcion

--- pagina_web/main.js
+++ pagina_web/main.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const counter = document.querySelector(".counter-number");
+const counter = document.querySelector(".counter-number");
 
-    // Muestra el número de visitantes que ha tenido la página accediendo a una funcion lambda que a su vez lee y incrementa un contador en una base de datos DynamoDB
-    async function updateCounter() {
-        let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
-        let data = await response.json();
-        counter.innerHTML = `You are visitor number ${data}!!`;
-        console.log(`Se ha actualizado el contador a ${data}`);
-    }
+// Muestra el número de visitantes que ha tenido la página accediendo a una funcion lambda que a su vez lee y incrementa un contador en una base de datos DynamoDB
+async function updateCounter() {
+    let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `You are visitor number ${data}!!`;
+    console.log(`Se ha actualizado el contador a ${data}`);
+}
 
-    updateCounter();
-});
+updateCounter();
