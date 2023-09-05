0000d6c7790ce024e6c16866b723bcf4c6d2e5ba
korah91 <62379054+korah91@users.noreply.github.com>
2023-09-05 16:36:15 +0200

    - arreglado el js

--- pagina_web/main.js
+++ pagina_web/main.js
@@ -1,8 +1,11 @@
 async function updateCounter() {
+    // Se hace una peticion GET a la funcion lambda
     let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
+    // Se obtiene el valor de la respuesta. El contador de visitas
     let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    // Se muestra el valor del contador en la consola
     console.log(`Se ha visitado la pagina ${data} veces`)
+    // Se crea un elemento al final del body con el valor del contador
     const counter = document.createElement("p");
     counter.textContent = "This page has received 5 visits. Each time someone visits this page, a JavaScript file sends a GET request to an AWS Lambda function, which in turn increments the counter in DynamoDB and reads the total value.";
     document.body.appendChild(counter);
