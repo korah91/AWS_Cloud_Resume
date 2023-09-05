dbe147661593798d56cc5a094444606d922a4687
korah91 <62379054+korah91@users.noreply.github.com>
2023-09-05 16:12:11 +0200

    - js

--- pagina_web/main.js
+++ pagina_web/main.js
@@ -1,11 +1,8 @@
 const counter = document.querySelector(".counter-number");
-
-// Muestra el número de visitantes que ha tenido la página accediendo a una funcion lambda que a su vez lee y incrementa un contador en una base de datos DynamoDB
 async function updateCounter() {
     let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
     let data = await response.json();
-    counter.innerText = `You are visitor number ${data}!!`;
-    console.log(`Se ha actualizado el contador a ${data}`);
+    counter.innerHTML = ` This page has ${data} Views!`;
 }
 
 updateCounter();
