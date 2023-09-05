55b9f0914c3782b9de0a6d39206e90ec057b2400
korah91 <62379054+korah91@users.noreply.github.com>
2023-09-05 16:23:55 +0200

    - js

--- pagina_web/main.js
+++ pagina_web/main.js
@@ -3,6 +3,7 @@ async function updateCounter() {
     let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
+    console.log(`Se ha visitado la pagina ${data} veces`)
 }
 
 updateCounter();
