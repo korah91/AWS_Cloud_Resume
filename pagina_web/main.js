f13fe772d265b76dbff43dc9b901865509e54acc
korah91 <62379054+korah91@users.noreply.github.com>
2023-09-05 16:35:19 +0200

    - Arreglado el js

--- pagina_web/main.js
+++ pagina_web/main.js
@@ -1,12 +1,11 @@
-const counter = document.querySelector(".counter");
 async function updateCounter() {
     let response = await fetch("https://piyzribk5n4wmjlnbcv3gtdtzm0vepxn.lambda-url.eu-west-3.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
     console.log(`Se ha visitado la pagina ${data} veces`)
-    const newParagraph = document.createElement("p");
-    newParagraph.textContent = "This is a new paragraph added via JavaScript.";
-    document.body.appendChild(newParagraph);
+    const counter = document.createElement("p");
+    counter.textContent = "This page has received 5 visits. Each time someone visits this page, a JavaScript file sends a GET request to an AWS Lambda function, which in turn increments the counter in DynamoDB and reads the total value.";
+    document.body.appendChild(counter);
 }
 
 updateCounter();
